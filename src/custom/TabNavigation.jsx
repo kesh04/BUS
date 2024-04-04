d2ee63cf643e5e5047c2708fc3aaef5d67252761
keshana viyanga <101414619+kesh04@users.noreply.github.com>
2024-04-04 20:02:35 +0530

    changes

--- src/custom/TabNavigation.jsx
+++ src/custom/TabNavigation.jsx
@@ -3,41 +3,67 @@ import { View, StyleSheet, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../page/Home";
 import Profile from "../page/Profile";
-import { Entypo } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
+import { Entypo, FontAwesome, Fontisto } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import Search from "../page/Search";
-import { FontAwesome5 } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 const TabNavigation = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown:false,}}>
-      <Tab.Screen name="Home" component={Home}
-      options={{
-        tabBarLabel:"Home",
-        headerShown:false,
-        tabBarIcon:({color,size})=>(
-          <Entypo name="home" size={24} color="#1c4b3d" />
-        )
-
-    }}/>
-     <Tab.Screen name="Search" component={Search}
-       options={{
-        tabBarLabel:"Search",
-        tabBarIcon:({color,size})=>(
-          <Feather name="search" size={24} color="#1c4b3d" />
-        )
-
-    }} />
-      <Tab.Screen name="Profile" component={Profile}
-       options={{
-        tabBarLabel:"Setting",
-        tabBarIcon:({color,size})=>(
-          <FontAwesome5 name="user-circle" size={24} color="#1c4b3d"  />
-        )
-
-    }} />
-     
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Entypo name="home" size={24} color="#097969" />
+            ) : (
+              <Entypo
+                name="home"
+                size={24}
+                color="#808080"
+              />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarLabel: "Search",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <FontAwesome name="search" size={24} color="#097969" />
+            ) : (
+              <FontAwesome
+                name="search"
+                size={24}
+                color="#808080"
+              />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Setting",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Fontisto name="player-settings" size={24} color="#097969" />
+            ) : (
+              <Fontisto
+                name="player-settings"
+                size={24}
+                color="#808080"
+              />
+            ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,15 +1,41 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 import TabNavigation from "../custom/TabNavigation";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { getStatusBarHeight } from "react-native-status-bar-height";
 export default function Home() {
+  const navigation = useNavigation();
+  // useLayoutEffect(()=>{
+  //   navigation.setOptions({
+  //     headerShown: true,
+  //     title:'',
+  //     headerStyle:{
+  //       backgroundColor:'red',
+  //       height:100
+  //     }
+  //   })
+  // },[])
+
   return (
     <SafeAreaView>
-      <View>
+      <View style={styles.container}>
         <Text>keshan</Text>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingBottom: 30,
+    paddingTop: getStatusBarHeight(),
+  },
+  header: {
+    backgroundColor: "#286ef0",
+    height: 70,
+    alignItems: "flex-start",
+  },
+});
