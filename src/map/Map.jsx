f549995be5e348265c101245bed910c1a1c340a3
keshana viyanga <101414619+kesh04@users.noreply.github.com>
2024-06-21 20:08:20 +0530

    New

--- src/map/Map.jsx
+++ src/map/Map.jsx
@@ -1,73 +1,17 @@
-// import React, { useEffect, useState } from "react";
-// import { StyleSheet, View } from "react-native";
-// import MapView, { Marker } from "react-native-maps";
-// import * as Location from 'expo-location';
-
-// const Map = () => {
-//   const [region, setRegion] = useState(null);
-
-//   useEffect(() => {
-//     (async () => {
-//       let { status } = await Location.requestForegroundPermissionsAsync();
-//       if (status !== 'granted') {
-//         console.error('Permission to access location was denied');
-//         return;
-//       }
-
-//       let location = await Location.getCurrentPositionAsync({});
-//       setRegion({
-//         latitude: location.coords.latitude,
-//         longitude: location.coords.longitude,
-//         latitudeDelta: 0.0922,
-//         longitudeDelta: 0.0421,
-//       });
-//     })();
-//   }, []);
-
-//   return (
-//     <View style={styles.container}>
-//       {region && (
-//         <MapView
-//           style={styles.map}
-//           initialRegion={region}
-//           showsUserLocation={true}
-//         >
-//           <Marker
-//             coordinate={{ latitude: region.latitude, longitude: region.longitude }}
-//             title={"Your Location"}
-//           />
-//         </MapView>
-//       )}
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   map: {
-//     width: "100%",
-//     height: "100%",
-//   },
-// });
-
-// export default Map;
-
-
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from 'expo-location';
 
 const Map = () => {
   const [region, setRegion] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        console.error('Permission to access location was denied');
+        setErrorMsg('Permission to access location was denied');
         return;
       }
 
@@ -80,6 +24,10 @@ const Map = () => {
       });
     })();
   }, []);
+
+  if (errorMsg) {
+    return <Text>{errorMsg}</Text>;
+  }
 
   return (
     <View style={styles.container}>
